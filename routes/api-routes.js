3448eadd58b6d808958ad71003c8b50c9515cfa2
David Palmquist <dpalmquist84@gmail.com>
2017-10-10 11:45:57 -0400

    add schema | updated api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,20 @@
+
+var db = require("../models");
 module.exports = function(app) {
 
+    app.get("/api/users/", function(req, res) {
+        db.User.findAll({})
+        .then(function(dbUser) {
+          res.json(dbUser);
+        });
+      });
+
+    app.get("/api/events/", function(req, res) {
+        db.Event.findAll({})
+        .then(function(dbEvent) {
+          res.json(dbEvent);
+        });
+      });
 
 var accountSid = 'AC7e4e81cff80cf1d86872f2066ec1c675'; // Your Account SID from www.twilio.com/console
 var authToken = '572ee0ec91e3ff8e029b57e7b0a3ab71';   // Your Auth Token from www.twilio.com/console
@@ -9,6 +24,33 @@ var client = new twilio(accountSid, authToken);
 
 app.get("/api/register", function(req, res) {
     res.json(tableData);
+  });
+
+  app.post("/api/users", function(req, res) {
+    console.log(req.body);
+    db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+    })
+    .then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.post("/api/events", function(req, res) {
+    console.log(req.body);
+    db.Event.create({
+      Description: req.body.Description,
+      Address: req.body.Address,
+      Date: req.body.Date,
+      Image: req.body.Image,
+      Category: req.body.Category,
+    })
+    .then(function(dbUser) {
+      res.json(dbUser);
+    });
   });
 
 
@@ -28,10 +70,6 @@ app.post("/api/register", function(req, res) {
             console.log(message.sid)
         }
         
-    });
-})
-// })
-// .then((message) => console.log(message.sid, message.to))
-// })
-
+        });
+    })
 }
---
+++ schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS betterhood;
+
+CREATE DATABASE betterhood;
