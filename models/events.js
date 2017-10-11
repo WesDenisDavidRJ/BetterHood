cf099feab943caf94fa887a845065ce16694e9d1
David Palmquist <dpalmquist84@gmail.com>
2017-10-11 10:47:10 -0400

    changed default value for events

--- models/events.js
+++ models/events.js
@@ -24,7 +24,7 @@ module.exports = function(sequelize, DataTypes) {
       },
       category: {
         type: DataTypes.STRING,
-        defaultValue: "Construction"
+        defaultValue: "General Assistance"
       }
     });
     return Event;
