a6905f512137cc6525773b53b69f0fc32147a06f
Denis Molloy <denis.molloy@gmail.com>
2017-10-13 16:22:04 -0400

    updating server questions

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
 
-var sequelize = new Sequelize("BetterHood", "root", "P1rates1", {
+var sequelize = new Sequelize("betterhood", "root", "Dipshit12!!", {
   host: "localhost",
   dialect: "mysql",
   pool: {
