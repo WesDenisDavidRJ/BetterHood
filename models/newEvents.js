00754ea167b6851a7738df6d1bc66126d396d0b3
Wes Bubeck <wesbubeck20@gmail.com>
2017-10-12 20:46:21 -0400

    var bcrypt = require("bcrypt-nodejs");

--- models/newEvents.js
+++ models/newEvents.js
@@ -1,3 +1,4 @@
+var bcrypt = require("bcrypt-nodejs");
 // Dependencies
 // =============================================================
 
--- models/newUser.js
+++ models/newUser.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcrypt-nodejs");
+
 // Dependencies 
 // =============================================================
 
