a02fa76c149add31cc6ca522b67c515614e313f4
David Palmquist <dpalmquist84@gmail.com>
2017-10-16 09:53:40 -0400

    added wildcard html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,6 +23,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/login.html"));
       });
 
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+      });
+
 
      
 
